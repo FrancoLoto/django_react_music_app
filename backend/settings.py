aca8b3857b6eae8f3e360d4e9d64a462f169e9cc
Franco Loto <franco.loto1995@gmail.com>
2023-10-22 15:45:12 -0300

    spotify integrado

--- backend/settings.py
+++ backend/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
     'api',
     'frontend',
+    'spotify',
 ]
 
 MIDDLEWARE = [
---
+++ spotify/credentials.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "3d979e1a4dfb473fb22ad5af0c722a46"
+CLIENT_SECRET = "baa0a8ba6af9492093ea7ce8268eeec3"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
